38b479bc562cb16adfea3893db58f0500570336b
Alexandros Papanikos <92908848+Ale-py@users.noreply.github.com>
2025-06-14 20:55:23 +0300

    Update Part_2.py

--- Part_2.py
+++ Part_2.py
@@ -1,7 +1,6 @@
 # 11. Basic Calculator
 # Create a function that takes two numbers and a mathematical operator + - / * and will perform a calculation with the given numbers.
 # If the input tries to divide by 0, return: "Can't divide by 0!"
-'''
 def calculator(num1,operator,num2):
     if num2 == 0:
         return  "Can't divide by 0!"
@@ -15,11 +14,9 @@ def calculator(num1,operator,num2):
         return num1 / num2
     
 print(calculator(0,"/",3))
-'''
 # 12. Calculating Damage
 # Create a function that takes damage and speed (attacks per second) and returns the amount of damage after a given time.
 # Return "invalid" if damage or speed is negative.
-'''
 def damage(dmg,speed,time):
     total = 0
     while dmg > 0 and speed > 0:
@@ -34,13 +31,11 @@ def damage(dmg,speed,time):
         return "invalid"
 
 print(damage(2, 0, "hour"))
-'''
 # 13. Let's Sort This List!
 # Create a function that takes a list of numbers lst, a string s and return a list of numbers as per the following rules:
 # "Asc" returns a sorted list in ascending order.
 # "Des" returns a sorted list in descending order.
 # "None" returns a list without any modification.
-'''
 def asc_des_none(nums,order):
     numbers_list = nums
     if order == "Asc":
@@ -52,11 +47,9 @@ def asc_des_none(nums,order):
     return numbers_list
 
 print(asc_des_none([1, 2, 3, 4], "None"))
-'''
 # 14. Return the Factorial
 # Create a function that takes an integer and returns the factorial of that integer. That is, the integer multiplied by all positive lower integers.
 # Assume all inputs are greater than or equal to 0.
-'''
 def factorial(num):
     total = 1
     for x in range(1,num+1):
@@ -64,15 +57,12 @@ def factorial(num):
     return total
 
 print(factorial(13))
-'''
-'''
+
 import math
 print(f"The factorial of 23 is : {math.factorial(23)}")
-'''
 # 15. Number Split
 # Given a number, return a list containing the two halves of the number. If the number is odd, make the rightmost number higher.
 # All numbers will be integers.You can expect negative numbers too.
-'''
 import math
 
 def number_split(num):
@@ -89,10 +79,7 @@ def number_split(num):
         return num_list
 
 print(number_split(-9))
-'''
 
-
-#✅
 
 
 
